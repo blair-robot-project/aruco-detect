083ff25409e7483fba52ad3988c916f74162bd43
Noah Gleason <noah.gleason1@gmail.com>
2018-02-10 22:49:43 -0500

    Fix cmake arguments

--- installOpenCV.sh
+++ installOpenCV.sh
@@ -27,7 +27,7 @@ apt install -y libopencore-amrnb-dev libopencore-amrwb-dev
 apt install -y x264 v4l-utils
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 make -j$(nproc)
 make install
 ldconfig
