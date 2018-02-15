427779b1eb6cfc873316ae969774048093fd7f9b
Natenael Kelkay <natikelkay@gmail.com>
2018-02-14 21:54:33 -0500

    Changed a few thing in the example codes

--- aruco_test/charuco_board/detect_board_charuco.cpp
+++ aruco_test/charuco_board/detect_board_charuco.cpp
@@ -1,42 +1,3 @@
-/*
-By downloading, copying, installing or using the software you agree to this
-license. If you do not agree to this license, do not download, install,
-copy or use the software.
-
-                          License Agreement
-               For Open Source Computer Vision Library
-                       (3-clause BSD License)
-
-Copyright (C) 2013, OpenCV Foundation, all rights reserved.
-Third party copyrights are property of their respective owners.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-  * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
-
-  * Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-
-  * Neither the names of the copyright holders nor the names of the contributors
-    may be used to endorse or promote products derived from this software
-    without specific prior written permission.
-
-This software is provided by the copyright holders and contributors "as is" and
-any express or implied warranties, including, but not limited to, the implied
-warranties of merchantability and fitness for a particular purpose are
-disclaimed. In no event shall copyright holders or contributors be liable for
-any direct, indirect, incidental, special, exemplary, or consequential damages
-(including, but not limited to, procurement of substitute goods or services;
-loss of use, data, or profits; or business interruption) however caused
-and on any theory of liability, whether in contract, strict liability,
-or tort (including negligence or otherwise) arising in any way out of
-the use of this software, even if advised of the possibility of such damage.
-*/
-
-
 #include <opencv2/highgui.hpp>
 #include "opencv2/aruco/charuco.hpp"
 
@@ -106,7 +67,6 @@ static bool readDetectorParameters(string filename, Ptr<aruco::DetectorParameter
 	fs["errorCorrectionRate"] >> params->errorCorrectionRate;
 	return true;
 }
-
 
 /**
  */
@@ -212,8 +172,6 @@ int main(int argc, char *argv[]) {
 			validPose = aruco::estimatePoseCharucoBoard(charucoCorners, charucoIds, charucoboard,
 			                                            camMatrix, distCoeffs, rvec, tvec);
 
-
-
 		double currentTime = ((double)getTickCount() - tick) / getTickFrequency();
 		totalTime += currentTime;
 		totalIterations++;
@@ -221,8 +179,6 @@ int main(int argc, char *argv[]) {
 			cout << "Detection Time = " << currentTime * 1000 << " ms "
 			     << "(Mean = " << 1000 * totalTime / double(totalIterations) << " ms)" << endl;
 		}
-
-
 
 		// draw results
 		image.copyTo(imageCopy);
@@ -246,6 +202,5 @@ int main(int argc, char *argv[]) {
 		char key = (char)waitKey(waitTime);
 		if(key == 27) break;
 	}
-
 	return 0;
 }
